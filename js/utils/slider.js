e0f82eafa2fefecb1308b4560bbdffaaf585f05e
Hugo Leroux <hugolerouxmusic@gmail.com>
2022-12-15 09:18:16 +0100

    slider nav functional

--- js/utils/slider.js
+++ js/utils/slider.js
@@ -2,20 +2,51 @@ import media from "../photographer.js";
 import SliderMedium from "../factories/SliderMedium.js";
 
 const modal = document.getElementById('slider-modal');
+const mediumContainer = modal.querySelector('.medium-container');
 const gallery = document.querySelector('.photograph-content-media');
+const title = document.querySelector('.slider-text');
 const close = document.querySelector('.slider-close');
+const prev = document.querySelector('.fa-chevron-left');
+const next = document.querySelector('.fa-chevron-right');
 
-function openSlider(medium) {
-  modal.style.display = 'block';
-  console.log(medium);
-
-  const mediumContainer = modal.querySelector('.medium-container');
-  const sliderMedium = new SliderMedium(medium);
+function createMedium(medium, index) {
+  const sliderMedium = new SliderMedium(medium, index);
   mediumContainer.innerHTML = sliderMedium.displayMedium();
+  title.innerText = medium.title;
+}
+
+function openSlider(medium, index) {
+  close.focus();
+  modal.style.display = 'block';
+  createMedium(medium, index);
 }
 
 function closeSlider() {
   modal.style.display = 'none';
+}
+
+function changeMedium(value) {
+  const currentMedium = document.querySelector('.slider-medium');
+  let index = Number(currentMedium.dataset.index);
+  console.log('current index is ', index);
+  if (value === 0) {
+    console.log('value is 0 no loop');
+    index -= 1;
+    if (index < 0) {
+      console.log('value is 0 looped');
+      index = media.length - 1;
+    }
+  }
+  if (value === 1) {
+    console.log('value is 1 no loop');
+    index += 1;
+    if (index > media.length - 1) {
+      console.log('value is 1 no looped');
+      index = 0;
+    }
+  }
+  console.log('new index is', index);
+  createMedium(media[index], index);
 }
 
 close.addEventListener('click', closeSlider);
@@ -24,8 +55,13 @@ gallery.addEventListener('click', (e) => {
   if (e.target.attributes.class.value === 'image-box-media') {
     const id = e.target.getAttribute('data-id');
     const index = media.findIndex((medium) => medium.id === Number(id));
-    // console.log(index);
-    // console.log(media[index]);
-    openSlider(media[index]);
+    openSlider(media[index], index);
   }
+});
+
+prev.addEventListener('click', () => {
+  changeMedium(0);
+});
+next.addEventListener('click', () => {
+  changeMedium(1);
 });
